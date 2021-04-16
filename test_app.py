6a8ff99d79d53f98618aefcae2d0a0c184f7ad51
sarojbe <31999674+sarojbe@users.noreply.github.com>
2021-04-15 21:51:39 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -12,5 +12,5 @@ def test_home():
     # Create a test client using the Flask application configured for testing
     with app.test_client() as test_client:
         response = test_client.get("/")
-        assert response.status_code == 200
+        assert response.status_code == 404
         assert b"Welcome to the app world!" not in response.data
